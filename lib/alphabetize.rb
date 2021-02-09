46dc9bc95d9c391453f66d5cea870d07b148aefd
pianoFF <fangfei1star@gmail.com>
2021-02-09 18:13:14 -0500

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -4,7 +4,7 @@ def alphabetize(arr)
   # code here
   word_ind = arr.map{|a| [a.split("").map{ |char| ESPERANTO_ALPHABET.index(char)}, a] }
   # binding.pry
-  result = []
-  word_ind.sort.each{|ary| result.push(ary[1])}
-  result
+  # result = []
+  word_ind.sort.map{|ary| ary[1]}
+  # result
 end
